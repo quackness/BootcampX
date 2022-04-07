031c6a4ad0247cb27824135192ff4ed3329c252f
Karolina Redden <dubajkaro@gmail.com>
2022-04-07 20:11:12 +0000

    changed to count(*) >= 10

--- 3_queries_group-by/2_total_assignments_greater_or_equal_10.sql
+++ 3_queries_group-by/2_total_assignments_greater_or_equal_10.sql
@@ -1,8 +1,8 @@
 --return rows where total assignments is greater than or equal to 10.
-SELECT day, COUNT(assignments.*) AS total_assignments
+SELECT day, count(*) as total_assignments 
 FROM assignments
 GROUP BY day
-HAVING COUNT(assignments.*) >= 10
+HAVING count(*) >= 10
 ORDER BY day;
 
 --The HAVING clause works on aggregated data, is evaluated before the SELECT so we can't use the alias 
