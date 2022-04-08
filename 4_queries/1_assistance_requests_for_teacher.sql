3e94e1e0f2f3fcf94eb5d2b7564ecf9a65b02369
Karolina Redden <dubajkaro@gmail.com>
2022-04-08 15:42:52 +0000

    total number of assistance_requests for a teacher

---
+++ 4_queries/1_assistance_requests_for_teacher.sql
@@ -0,0 +1,14 @@
+/*Get the total number of assistance_requests for a teacher.
+Select the teacher's name and the total assistance requests.
+Since this query needs to work with any specific teacher name, 
+use 'Waylon Boehm' for the teacher's name here.*/
+
+SELECT count(assistance_requests.*) as  total_assistances, 
+teachers.name as name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
+
+
+
