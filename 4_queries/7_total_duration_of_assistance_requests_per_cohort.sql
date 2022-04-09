995855237080345f6e0a6039906b4008cdcd8940
Karolina Redden <dubajkaro@gmail.com>
2022-04-09 00:12:14 +0000

    formatting changes

--- 4_queries/7_total_duration_of_assistance_requests_per_cohort.sql
+++ 4_queries/7_total_duration_of_assistance_requests_per_cohort.sql
@@ -3,8 +3,7 @@ Select the cohort's name and the total duration the assistance requests.
 Order by total_duration.
 Look at the ERD to see how to join the tables.*/
 
-SELECT cohorts.name as cohort,
-sum(completed_at-started_at) as total_duration
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts on cohorts.id = cohort_id
