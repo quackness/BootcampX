cb868d28d1df3637e7280393a047ccdaa2e0be8a
Karolina Redden <dubajkaro@gmail.com>
2022-04-13 16:39:23 +0000

    added Parameterized Query to prevent sql attacks

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,26 +11,26 @@ const pool = new Pool({
 //(res.rows) returns the database results are being 
 //represented as an array of JavaScript objects.
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   console.log(res.rows);
+// })
+// .catch(err => console.error('query error', err.stack));
 
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
-  })
-});
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// });
 
 
 
@@ -50,16 +50,37 @@ LIMIT 5;
 
 //Query Parameters
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name AS cohort
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   console.log(res)
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+
+//Parameterized Query
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+const cohortName = process.argv[2];
+const limit = Number(process.argv[3]) || 5;//NUMBER!!
+// Store all potentially malicious values in an array.
+
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.log('query error', err.stack));
-
+}).catch(err => console.error('query error', err.stack));
