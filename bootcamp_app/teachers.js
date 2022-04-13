56c6bab738f646f96a873a85d7be2174ad66c7aa
Karolina Redden <dubajkaro@gmail.com>
2022-04-13 14:51:05 +0000

    qr for name of teachers that assisted using a promise and argv

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,10 +14,14 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
 ORDER BY teacher;
 `)
 .then(res => {
-  console.log(res.rows);
+  //console.log(res.rows); used to print entire object
+  res.rows.forEach(user => {
+    console.log(`${user.teacher} has assisted ${user.cohort}`)
+    console.log(`${user.cohort}: ${user.teacher}`)
+  })
 })
 .catch(err => console.error('query error', err.stack));
