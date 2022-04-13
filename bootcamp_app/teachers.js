d8875b26b785c01c12bc80aa9d2a1cf459a193cd
Karolina Redden <dubajkaro@gmail.com>
2022-04-13 14:54:39 +0000

    argv condition adjusted, added OR statement as per the cprrect answer in compass

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,13 +14,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
 `)
 .then(res => {
   //console.log(res.rows); used to print entire object
   res.rows.forEach(user => {
-    console.log(`${user.teacher} has assisted ${user.cohort}`)
     console.log(`${user.cohort}: ${user.teacher}`)
   })
 })
