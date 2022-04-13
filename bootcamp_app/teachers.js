2a2301d6df57e410fb68a3b844d6fd2b6f14e5ab
Karolina Redden <dubajkaro@gmail.com>
2022-04-13 18:20:22 +0000

    Parameterized Query added to the query using promise

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+const cohortName = process.argv[2];
+const values = [`%${cohortName || 'JUL02'}%`];
+
+pool.query(queryString, values)
 .then(res => {
-  //console.log(res.rows); used to print entire object
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
   })
 })
 .catch(err => console.error('query error', err.stack));
+
+
